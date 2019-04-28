55ec2c596551d0885806bf804d205d7b67fbd3fd
aoba <ipbdipbd00@gmail.com>
2019-04-28 14:18:50 +0900

    letを使ったtask modelのspecを作成

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:project) { FactoryBot.create(:project) }
+
+  it "プロジェクトと名前があれば有効な状態であること" do
+    task = Task.new(
+      project: project,
+      name: "New Task"
+    )
+    expect(task).to be_valid
+  end
+
+  it "プロジェクトがなければ無効な状態であること" do
+    task = Task.new(project: nil)
+    task.valid?
+    expect(task.errors[:project]).to include "must exist"
+  end
+
+  it "タスクの名前がなければ無効な状態であること" do
+    task = Task.new(name: nil)
+    task.valid?
+    expect(task.errors[:name]).to include "can't be blank"
+  end
 end
