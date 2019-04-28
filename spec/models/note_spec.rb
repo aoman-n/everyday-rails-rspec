a9fdc911f5d6ea37471288a1b028a1b795f58687
aoba <ipbdipbd00@gmail.com>
2019-04-28 15:07:31 +0900

    noteモデルのテストをletでリファクタリング

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,71 +2,58 @@ require 'rails_helper'
 
 RSpec.describe Note, type: :model do
 
-  it "ファクトリで関連するデータを生成する" do
-    note = FactoryBot.create(:note)
-    puts "note project -> #{note.project.owner.inspect}"
-    puts "note user -> #{note.user.inspect}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
+
+  it "ユーザー、プロジェクト、メッセージがあれば有効な状態であること" do
+    note = Note.new(
+      message: "This is a sample note.",
+      project: project,
+      user: user
+    )
+    expect(note).to be_valid
   end
 
-  # before do
-  #   # 全体の前処理
-  #   @user = User.create(
-  #     first_name:"Joe",
-  #     last_name: "Tester",
-  #     email: "joetester@example.com",
-  #     password: "dottle-nouveau-pavilion-tights-furze",
-  #   )
-  #   @project = @user.projects.create(
-  #     name: "Test Project",
-  #   )
-  # end
+  it "メッセージがなければ無効な状態であること" do
+    note = Note.new(message: nil)
+    note.valid?
+    expect(note.errors[:message]).to include "can't be blank"
+  end
 
-  # it "ユーザー、プロジェクト、メッセージがあれば有効な状態であること" do
-  #   note = Note.new(
-  #     message: "This is a sample note.",
-  #     project: @project,
-  #     user: @user
-  #   )
-  #   expect(note).to be_valid
-  # end
+  describe "文字列に一致するメッセージを検索する" do
+    let!(:note1) {
+      FactoryBot.create(:note,
+        message: "First Note, yeah",
+        project: project,
+        user: user
+      )
+    }
+    let!(:note2) {
+      FactoryBot.create(:note,
+        message: "Second Note",
+        project: project,
+        user: user
+      )
+    }
+    let!(:note3) {
+      FactoryBot.create(:note,
+        message: "Third Note, yeah",
+        project: project,
+        user: user
+      )
+    }
 
-  # it "メッセージがなければ無効な状態であること" do
-  #   note = Note.new(
-  #     message: "",
-  #     project: @project,
-  #     user: @user
-  #   )
-  #   expect(note).to be_invalid
-  # end
+    context "一致するメッセージがあるとき" do
+      it "検索文字列に一致するメモを返すこと" do
+        expect(Note.search("yeah")).to include(note1, note3)
+      end
+    end
 
-  # describe "文字列に一致するメッセージを検索する" do
+    context "一致するメッセージが1件もないとき" do
+      it "空のコレクションを返すこと" do
+        expect(Note.search("message")).to be_empty
+      end
+    end
 
-  #   before do
-  #     @note1 = @project.notes.create(
-  #       message: "This is the first note.",
-  #       user: @user,
-  #     )
-  #     @note2 = @project.notes.create(
-  #       message: "This is the second note.",
-  #       user: @user,
-  #     )
-  #     @note3 = @project.notes.create(
-  #       message: "First, preheat the oven.",
-  #       user: @user,
-  #     )
-  #   end
-
-  #   context "一致するメッセージがあるとき" do
-  #     it "検索文字列に一致するメモを返すこと" do
-  #       expect(Note.search("first")).to include(@note1, @note3)
-  #     end
-  #   end
-
-  #   context "一致するメッセージが1件もないとき" do
-  #     it "空のコレクションを返すこと" do
-  #       expect(Note.search("message")).to be_empty
-  #     end
-  #   end
-
-  # end
+  end
 end
